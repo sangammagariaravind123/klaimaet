f15c54b78297e328c121442bf5b14b351fe8aa77
Aravind Chandra <66818266+sangammagariaravind123@users.noreply.github.com>
2025-10-08 13:44:13 +0530

    gh--pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/",
+  base: process.env.NODE_ENV === 'production' ? '/klaimaet/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
